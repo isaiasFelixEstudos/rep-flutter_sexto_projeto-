f16462520bcf3d9d48599e32d523a03480ca6bc0
Isaias Félix Machado de Alcantara <133122614+isaiasFelixEstudos@users.noreply.github.com>
2023-05-29 15:15:18 -0400

    gdfg

--- sixproject/lib/pg/logado.dart
+++ sixproject/lib/pg/logado.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Logado extends StatefulWidget {
@@ -7,13 +8,25 @@ class Logado extends StatefulWidget {
   State<Logado> createState() => _LogadoState();
 }
 
+var retorno = '';
+
 class _LogadoState extends State<Logado> {
+  Future<void> validarLogado() async {
+    var user = await FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      retorno = 'Usuário logado é: ${user.email}';
+    } else {
+      retorno = 'Nenhum usuário logado';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Página logada')),
       body: Column(children: [
         Text('Logado'),
+        Text(retorno),
       ]),
     );
   }
